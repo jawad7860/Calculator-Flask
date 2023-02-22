8cbac0313159e44406cba1f63b1520e7213b1f73
jawad7860 <afzaljawad297@gmail.com>
2023-02-22 10:30:00 +0500

    update

---
+++ sub.py
@@ -0,0 +1,16 @@
+"""
+This module defines a function to subtracts two numbers.
+"""
+
+def sub(x_value, y_value):
+    """
+    Adds two numbers and returns the result.
+
+    Args:
+        x_value (int): The first number.
+        y_value (int): The second number.
+
+    Returns:
+        int: The sum of x_value and y_value.
+    """
+    return x_value - y_value
